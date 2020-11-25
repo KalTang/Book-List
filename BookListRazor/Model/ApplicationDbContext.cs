6632d59db6b47b84dec068c2a39ecb56bc4c799c
Kalvin Tang <ktang82@my.bcit.ca>
2020-11-25 05:59:35 -0800

    Created models, created book database

---
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // for dependency injections
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //book model to database
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
